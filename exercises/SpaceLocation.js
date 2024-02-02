1ea8e4f929aad78745102d53b807bb8856adda0a
rishikeus <rishik.bhimavarapu@keus.in>
2024-02-02 12:33:37 +0530

    done

---
+++ exercises/SpaceLocation.js
@@ -0,0 +1,12 @@
+"use strict";
+// Paste in the provided code here:
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SpaceLocation = void 0;
+var SpaceLocation = /** @class */ (function () {
+    function SpaceLocation(name, kilometersAway) {
+        this.name = name;
+        this.kilometersAway = kilometersAway;
+    }
+    return SpaceLocation;
+}());
+exports.SpaceLocation = SpaceLocation;
